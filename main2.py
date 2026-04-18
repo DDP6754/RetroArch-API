493d13296abba17b233e12b0a606ef86c964fa5b
Alex <alex@ejemplo.com>
2026-04-18 23:03:17 +0200

    Codigo del servidor con problemas en websocket

---
+++ main2.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
+from routers import scrapper, usuarios, catalogo, partidas
+from database import init_db
+from contextlib import asynccontextmanager
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await init_db()
+    yield
+
+app = FastAPI(title="RetroArch API", lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permite que Tauri (o cualquier frontend) se conecte
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+#Con esto se puede acceder al contenido de /storage con: http://127.0.0.1:8000/storage/....
+app.mount("/storage", StaticFiles(directory="storage"), name="storage")
+
+app.include_router(usuarios.router)
+app.include_router(catalogo.router)
+app.include_router(partidas.router)
+app.include_router(scrapper.router)
--- populate_db.py
+++ populate_db.py
@@ -1,10 +1,8 @@
 import asyncio
 from sqlalchemy.future import select
 from database import AsyncSessionLocal, Consola, init_db
-RETRO_CONFIG = "~/.var/app/org.libretro.RetroArch/config/retroarch"
 CONSOLAS_INICIALES = [
-    {"console": "gba", "emulador": f"{RETRO_CONFIG}/cores/mgba_libretro.so"},
-
+    {"console": "gba", "emulador": "/ruta/emulador/gba"},
     {"console": "ds", "emulador": "/ruta/emulador/ds"},
     {"console": "gamecube", "emulador": "/ruta/emulador/gamecube"}
 ]
